cac34cd97f62451554aa025da661851f7e642382
Nisarg Ujjainkar <nisarg.ujjainkar@iitgn.ac.in>
2018-06-29 00:40:56 +0530

    a

--- DoAnIteration.py
+++ DoAnIteration.py
@@ -11,22 +11,25 @@ def DoAnIteration(nnums,T,n):
     if a>=(b*n):
         q=int(a/b)
         p=1
+        pair.append([p,q])
     else:
         while p<=ceil(n*b/a):
             if p!=0:
+                q=0-n
                 while q<=n:
-                    if q!=0:
-                      r=p*a-q*b
-                      if r>=0 and r<=int(2*a/n):
-                          pair.append([p,q])
+                    r=p*a-q*b
+                    if r>=0 and r<=int(2*a/n):
+                        print(r)
+                        pair.append([p,q])
+                    #print(p,'\t',q)
                     q=q+1
             p=p+1
     print (pair)
     t=[[1,0],[0,1]]
     t[0][0]=0*T[0][0]+1*T[1][0]
     t[0][1]=0*T[0][1]+1*T[1][1] 
-    t[1][0]=p*T[0][0]-q*T[1][0]
-    t[1][1]=p*T[0][1]-q*T[1][1]
+    t[1][0]=pair[0][0]*T[0][0]-pair[0][1]*T[1][0]
+    t[1][1]=pair[0][0]*T[0][1]-pair[0][1]*T[1][1]
     T[0][0]=t[0][0]
     T[1][0]=t[1][0]
     T[0][1]=t[0][1]
